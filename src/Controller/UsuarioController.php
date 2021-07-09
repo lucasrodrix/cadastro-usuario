4a37ea65ac7208b29040e5dd5f7bc9cc1d6f9ac0
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-07-09 16:46:47 -0300

    Implementado os templates de ERRO e Sucesso, assim como o do formulario com Twig

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -1,22 +1,23 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/", name="web_usuario_")
  */
-class UsuarioController{
+class UsuarioController extends AbstractController{
     /**
      * @Route("/", methods={"GET"}, name="index")
      */
     public function index(): Response{
-        return new Response("implementar formulário de cadastro");
+        return $this->render("usuario/form.html.twig");
     }
     
     /**
-     * @Route("/salvar", methods={"POST"}, name="salva")
+     * @Route("/salvar", methods={"POST"}, name="salvar")
      */
     public function salvar(): Response{
         return new Response("implementar gravação no BD");
