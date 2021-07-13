f6d5a383de487b8b5c444a499263c0efdffae30f
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-07-13 11:56:21 -0300

    Correção da função de cadastrar o usuario

--- src/Controller/Api/UsuarioController.php
+++ src/Controller/Api/UsuarioController.php
@@ -2,24 +2,45 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Usuario;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("api/v1", name="api_v1_usuario_")
  */
-class UsuarioController{
+class UsuarioController extends AbstractController{
+    
     /**
      * @Route("/listar", methods={"GET"}, name="lista")
      */
     public function listar(): JsonResponse{
-        return new JsonResponse(["Implementar listagem na API"], 404);
+        $doctrine = $this->getDoctrine()->getRepository(Usuario::class);
+
+        return new JsonResponse($doctrine->getAll());
     }
 
     /**
      * @Route("/cadastrar", methods={"POST"}, name="cadastro")
      */
-    public function cadastrar(): JsonResponse{
-        return new JsonResponse(["Implementar cadastro na API"], 404);
+    public function cadastrar(Request $request): Response{
+        $data = $request->query->all();
+
+        $usuario = new Usuario;
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($usuario);
+        $doctrine->flush();
+
+        if($doctrine->contains($usuario)){
+            return new Response("Ok",200);
+        }else{
+            return new Response("não Ok", 404);
+        }
     }
 }
